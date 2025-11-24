f02bc0fe7a87c07ba959e8120001f92866874228
NirmitThakkar700 <np.thakkar244@gmail.com>
2025-11-24 03:50:55 -0500

    Update NoteSyncApp.kt

--- app/src/main/java/week11/st856364/finalproject/NoteSyncApp.kt
+++ app/src/main/java/week11/st856364/finalproject/NoteSyncApp.kt
@@ -1,1 +1,11 @@
 package week11.st856364.finalproject
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class NoteSyncApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
