2cf3f1d8fdf4a526049cf522c4d0313b4a6733dc
Vraj Contractor <vrajcontractor20@gmail.com>
2025-12-01 06:16:15 -0500

    FINAL

--- app/src/main/java/week11/st856364/finalproject/data/model/Note.kt
+++ app/src/main/java/week11/st856364/finalproject/data/model/Note.kt
@@ -6,8 +6,8 @@ data class Note(
     @DocumentId val id: String = "",
     val title: String = "",
     val content: String = "",
-    val color: Long = 0xFFFFFFFF,
-    val pinned: Boolean = false,
+    val color: Long = 0xFFFFFFFF,     // DEFAULT COLOR
+    val pinned: Boolean = false,      // DEFAULT PIN
     val timestamp: Long = System.currentTimeMillis(),
     val languageCode: String = "en"
 )
